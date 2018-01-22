742436ff52d04f708411fd285d9f76cf9ef8fee7
Tsegab Gebregziabher <tg29285@email.vccs.edu>
2018-01-22 16:19:30 -0500

    initial

---
+++ EvenNumbers.java
@@ -0,0 +1,16 @@
+
+public class EvenNumbers {
+
+		public static void main(String [] args){
+			int i =0;
+
+		do{
+			i++;
+			i++;
+		
+		System.out.println("EvenNumbers between 1 and 100 are:"+i);
+
+		}while(i<100);
+		
+	}
+}
